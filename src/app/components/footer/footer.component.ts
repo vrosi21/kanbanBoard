c17b32222571e5b845a4ec3c8868776c9408a622
Kemal Sivro <kemalsivro@gmail.com>
2024-04-15 18:24:31 +0200

    about and register page created

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+	constructor(private router: Router) {}
 
+	navigateTo(route: string): void {
+		this.router.navigateByUrl(route).then(() => {
+			window.scrollTo(0, 0);
+		});
+	}
 }
